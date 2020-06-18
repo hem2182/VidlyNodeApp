c67d976f3b06b3e548b5dbfa49134061ba753a4d
hem2182 <hemant_2182_fresco@yahoo.com>
2020-06-18 13:16:09 +0530

    Added persistance layer for genre api for mongoDB

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-console.log("Before");
-setTimeout(() => {
-  console.log("After 2 seconds, this function is called. ");
-}, 2000);
-console.log("After");
+require("dotenv").config();
+const { startupDebugger, dbDebugger } = require("./middleware/debuggerConfig");
+const config = require("config");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const logger = require("./middleware/logger");
+const genres = require("./routes/genres");
+const home = require("./routes/home");
+const mongoose = require("mongoose");
 
-// require("dotenv").config();
-// const { startupDebugger, dbDebugger } = require("./middleware/debuggerConfig");
-// const config = require("config");
-// const morgan = require("morgan");
-// const helmet = require("helmet");
-// const logger = require("./middleware/logger");
-// const genres = require("./routes/genres");
-// const home = require("./routes/home");
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// // middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-// app.use(helmet());
+// connecting to mongoDB
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Vidly Database..."))
+  .catch((error) => console.log("Could not connect to MongoDB.", error));
 
-// // logger
-// app.use(logger);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
 
-// // environments
-// if (app.get("env") === "development") {
-//   startupDebugger("Morgan Enabled...");
-//   app.use(morgan("tiny"));
-// }
+// logger
+app.use(logger);
 
-// // Configuration
-// console.log("Application Name " + config.get("name"));
-// console.log("Application Mail Server " + config.get("mail.host"));
-// console.log("Application Mail Password: " + config.get("mail.password"));
+// environments
+if (app.get("env") === "development") {
+  startupDebugger("Morgan Enabled...");
+  app.use(morgan("tiny"));
+}
 
-// // DB Work.
-// dbDebugger("Connected to the Database...");
+// Configuration
+console.log("Application Name " + config.get("name"));
+console.log("Application Mail Server " + config.get("mail.host"));
+console.log("Application Mail Password: " + config.get("mail.password"));
 
-// // routes
-// app.use("/api/genres", genres);
-// app.use("/", home);
+// DB Work.
+dbDebugger("Connected to the Database...");
 
-// // view engine
-// app.set("view engine", "pug");
-// app.set("views", "./views");
+// routes
+app.use("/api/genres", genres);
+app.use("/", home);
 
-// // listener
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log(`Listening on port ${port}...`));
+// listener
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
