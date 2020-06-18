ecb83728c4d35aec56b9b97c39ddb7d2ec19e675
hem2182 <hemant_2182_fresco@yahoo.com>
2020-06-18 12:17:19 +0530

    learnt mongoDB using mongoose

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-require("dotenv").config();
-const { startupDebugger, dbDebugger } = require("./middleware/debuggerConfig");
-const config = require("config");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const logger = require("./middleware/logger");
-const genres = require("./routes/genres");
-const home = require("./routes/home");
-const express = require("express");
-const app = express();
+console.log("Before");
+setTimeout(() => {
+  console.log("After 2 seconds, this function is called. ");
+}, 2000);
+console.log("After");
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(helmet());
+// require("dotenv").config();
+// const { startupDebugger, dbDebugger } = require("./middleware/debuggerConfig");
+// const config = require("config");
+// const morgan = require("morgan");
+// const helmet = require("helmet");
+// const logger = require("./middleware/logger");
+// const genres = require("./routes/genres");
+// const home = require("./routes/home");
+// const express = require("express");
+// const app = express();
 
-// logger
-app.use(logger);
+// // middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.use(helmet());
 
-// routes
-app.use("/api/genres", genres);
-app.use("/", home);
+// // logger
+// app.use(logger);
 
-// view engine
-app.set("view engine", "pug");
-app.set("views", "./views");
+// // environments
+// if (app.get("env") === "development") {
+//   startupDebugger("Morgan Enabled...");
+//   app.use(morgan("tiny"));
+// }
 
-// Configuration
-console.log("Application Name " + config.get("name"));
-console.log("Application Mail Server " + config.get("mail.host"));
-console.log("Application Mail Password: " + config.get("mail.password"));
+// // Configuration
+// console.log("Application Name " + config.get("name"));
+// console.log("Application Mail Server " + config.get("mail.host"));
+// console.log("Application Mail Password: " + config.get("mail.password"));
 
-// environments
-if (app.get("env") === "development") {
-  startupDebugger("Morgan Enabled...");
-  app.use(morgan("tiny"));
-}
+// // DB Work.
+// dbDebugger("Connected to the Database...");
 
-// DB Work.
-dbDebugger("Connected to the Database...");
+// // routes
+// app.use("/api/genres", genres);
+// app.use("/", home);
 
-// listener
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// // view engine
+// app.set("view engine", "pug");
+// app.set("views", "./views");
+
+// // listener
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
